deb736680b6dee7d451beeff110aa640c18abaa3
krlnr <carelnehr.leo@gmail.com>
2015-08-20 16:30:23 +0200

    first pages

--- js/script.js
+++ js/script.js
@@ -9,3 +9,9 @@ $('.more-button a[href]').on('click', function(e){
     var link = $(this).attr('href');
     $('body').animate({ scrollTop: $(link).offset().top }, 1000);
 });
+
+
+$(".step_1").hover(function(){
+    $(".step_2").css("visibility","visible", "slow")});
+$(".step_1").mouseleave(function(){
+    $(".step_2").css("visibility","hidden", "slow")});
